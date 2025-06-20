d297e024f5216785c1f9e59ba1778aab770fb79a
Orion Alves <oriondecarvalho@gmail.com>
2025-06-19 21:54:01 -0300

    TODO: Implementar as propriedades faltantes de acordo com o diagrama.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,7 +3,9 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo;
+        protected string IMAI;
+        protected int Memoria;
 
         public Smartphone(string numero)
         {
